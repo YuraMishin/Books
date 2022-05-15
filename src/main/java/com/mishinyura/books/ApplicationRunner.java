2fa9d95ac8f0b26d317a104264582429a296c630
Mishin Yura <mishin.inbox@gmail.com>
2022-05-15 09:32:23 +0300

    Add Lombok

--- src/main/java/com/mishinyura/books/ApplicationRunner.java
+++ src/main/java/com/mishinyura/books/ApplicationRunner.java
@@ -1,12 +1,15 @@
 package com.mishinyura.books;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class ApplicationRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationRunner.class, args);
+        log.info("Hello, Yura!");
     }
 }
