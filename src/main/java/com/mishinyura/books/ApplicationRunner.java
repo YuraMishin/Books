90a48ee89aa6a265f403ca941a21d2c9c921b2f7
Mishin Yura <mishin.inbox@gmail.com>
2022-05-16 18:41:40 +0300

    Add Postgresql database and display its name. Closes #10

--- src/main/java/com/mishinyura/books/ApplicationRunner.java
+++ src/main/java/com/mishinyura/books/ApplicationRunner.java
@@ -1,5 +1,6 @@
 package com.mishinyura.books;
 
+import com.mishinyura.books.util.DBUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,5 +12,6 @@ public class ApplicationRunner {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationRunner.class, args);
         log.info("Hello, Yura!");
+        log.info("Your database is: {}", DBUtils.getDBName());
     }
 }
