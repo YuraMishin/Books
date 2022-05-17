9670cc1d3051451b5eb318d5a342105d5c9aab66
Mishin Yura <mishin.inbox@gmail.com>
2022-05-17 09:37:44 +0300

    Added feature to get JDBC Connection to database. Closes #14

--- src/main/java/com/mishinyura/books/ApplicationRunner.java
+++ src/main/java/com/mishinyura/books/ApplicationRunner.java
@@ -1,9 +1,11 @@
 package com.mishinyura.books;
 
-import com.mishinyura.books.util.DBUtils;
+import com.mishinyura.books.db.ConnectionManager;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.sql.Connection;
 
 @SpringBootApplication
 @Slf4j
@@ -12,6 +14,11 @@ public class ApplicationRunner {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationRunner.class, args);
         log.info("Hello, Yura!");
-        log.info("Your database is: {}", DBUtils.getDBName());
+
+        try (Connection con = ConnectionManager.open()) {
+            log.info("Your database is: {}", con.getCatalog());
+        } catch (Exception e) {
+
+        }
     }
 }
