d894f9e8de1c7f7917ed24c8ec7ed3870b1a6562
Mishin Yura <mishin.inbox@gmail.com>
2022-05-17 15:33:23 +0300

    Add BookDao. Implement save book feature. Closes #20

---
+++ src/main/java/com/mishinyura/books/db/SqlQueries.java
@@ -0,0 +1,23 @@
+package com.mishinyura.books.db;
+
+/**
+ * Class SqlQueries.
+ * Implements Sql Queries storage.
+ *
+ * @author Mishin Yura (mishin.inbox@gmail.com)
+ * @since 17.05.2022
+ */
+public final class SqlQueries {
+    /**
+     * Constructor.
+     */
+    private SqlQueries() {
+    }
+
+    /**
+     * Insert a book into Books Table.
+     */
+    public static final String INSERT_BOOK = """
+            INSERT INTO books (title) VALUES (?);
+            """;
+}
--- src/main/java/com/mishinyura/books/domain/Book.java
+++ src/main/java/com/mishinyura/books/domain/Book.java
@@ -1,27 +1,77 @@
 package com.mishinyura.books.domain;
 
+/**
+ * Class Book.
+ * Implements Book.
+ *
+ * @author Mishin Yura (mishin.inbox@gmail.com)
+ * @since 17.05.2022
+ */
 public class Book {
+    /**
+     * Id.
+     */
     private Long id;
+
+    /**
+     * Title.
+     */
     private String title;
 
-    public Book(Long id, String title) {
+    /**
+     * Constructor.
+     */
+    public Book() {
+    }
+
+    /**
+     * Constructor.
+     */
+    public Book(String title) {
+        this.title = title;
+    }
+
+    /**
+     * Constructor.
+     */
+    public Book(final Long id, final String title) {
         this.id = id;
         this.title = title;
     }
 
-    public Long getId() {
-        return id;
-    }
-
+    /**
+     * @return Title
+     */
     public String getTitle() {
         return title;
     }
 
-    public void setId(Long id) {
+    /**
+     * @param title Title
+     */
+    public void setTitle(final String title) {
+        this.title = title;
+    }
+
+    /**
+     * @param id Id
+     */
+    public void setId(final Long id) {
         this.id = id;
     }
 
-    public void setTitle(final String title) {
-        this.title = title;
+    /**
+     * @return Id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                '}';
     }
 }
