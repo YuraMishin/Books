8f8695615f95020809c29902a2ce575b03aca87f
Mishin Yura <mishin.inbox@gmail.com>
2022-05-30 11:45:54 +0300

    Loaded initial data via CommandLineRunner Interface. Closes #40

---
+++ src/main/java/com/mishinyura/books/bootstrap/BootstrapData.java
@@ -0,0 +1,37 @@
+package com.mishinyura.books.bootstrap;
+
+import com.mishinyura.books.entity.BookV2;
+import com.mishinyura.books.repository.BookRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Class BootstrapData.
+ * Implements initial data loading.
+ *
+ * @author Mishin Yura (mishin.inbox@gmail.com)
+ * @since 30.05.2022
+ */
+@RequiredArgsConstructor
+@Slf4j
+@Component
+public class BootstrapData implements CommandLineRunner {
+    /**
+     * Book Repository.
+     */
+    private final BookRepository bookRepository;
+
+    /**
+     * Method runs bootstrap code.
+     *
+     * @param args Args
+     */
+    @Override
+    public void run(final String... args) {
+        bookRepository.save(new BookV2("title1CLR"));
+        bookRepository.save(new BookV2("title2CLR"));
+        log.info("Loaded: {} book(s)", bookRepository.count());
+    }
+}
