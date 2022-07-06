822c39ab6ec49035aed6451616d422c0330d6ac6
Mishin Yura <mishin.inbox@gmail.com>
2022-07-06 11:34:01 +0300

    Handled BookNotFoundException. Closes #119

---
+++ src/main/java/com/mishinyura/books/exceptions/BookNotFoundException.java
@@ -0,0 +1,11 @@
+package com.mishinyura.books.exceptions;
+
+/**
+ * Class BookNotFoundException.
+ * Implements Book Not Found Exception.
+ *
+ * @author Mishin Yura (mishin.inbox@gmail.com)
+ * @since 06.07.2022
+ */
+public class BookNotFoundException extends RuntimeException {
+}
